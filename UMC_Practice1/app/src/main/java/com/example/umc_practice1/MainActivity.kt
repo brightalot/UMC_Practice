056e18ce9db5c4fffe297fbdbe5382e85afe4640
brightalot <gamja9151@hanyang.ac.kr>
2023-05-22 02:59:47 +0900

    bottomnavigationview 구현

---
+++ UMC_Practice1/app/src/main/java/com/example/umc_practice1/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.umc_practice1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.umc_practice1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.containerFragment.id, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.bottomNavigationView.run {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.menu_home -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.containerFragment.id, HomeFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_setting -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.containerFragment.id, SettingFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_mypage -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.containerFragment.id, MypageFragment())
+                            .commitAllowingStateLoss()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.menu_home
+        }
+    }
+}
